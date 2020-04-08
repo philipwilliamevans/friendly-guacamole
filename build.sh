46f8dc7ee7e552ca47d1109791729a13e9cfa11f
Phil Evans <phil.evans@thoughtworks.com>
2020-04-08 16:52:41 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 export TERM=${TERM:-dumb}
 # cp -rv animated-spork jar-files
 echo "Copying input files to output directory"
-cp -R animated-spork/. jar-files
-echo "Directory listing of animated-spork"
-ls -al animated-spork
+cp -R friendly-guacamole/. jar-files
+echo "Directory listing of friendly-guacamole"
+ls -al friendly-guacamole
 echo "Directory listing of jar-files"
 ls -al jar-files
 echo "CD"
